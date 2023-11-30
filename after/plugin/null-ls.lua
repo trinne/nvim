94a212e4c932380e92d8004493b36f843811719a
Tuomas <torinne@gmail.com>
2023-11-30 13:09:51 +0200

    Fix format timeout error

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -3,12 +3,13 @@ local null_ls = require("null-ls")
 local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
+local timeout = 10000
 
 null_ls.setup({
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
       vim.keymap.set("n", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), timeout = timeout})
         print("File formatted.")
       end, { buffer = bufnr, desc = "[lsp] format" })
 
@@ -18,7 +19,7 @@ null_ls.setup({
         buffer = bufnr,
         group = group,
         callback = function()
-          vim.lsp.buf.format({ bufnr = bufnr, async = async })
+          vim.lsp.buf.format({ bufnr = bufnr, async = async, timeout = timeout })
           print("File formatted on save.")
         end,
         desc = "[lsp] format on save",
@@ -27,7 +28,7 @@ null_ls.setup({
 
     if client.supports_method("textDocument/rangeFormatting") then
       vim.keymap.set("x", "<Leader>f", function()
-        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+        vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), timeout = timeout })
       end, { buffer = bufnr, desc = "[lsp] format" })
     end
   end,
