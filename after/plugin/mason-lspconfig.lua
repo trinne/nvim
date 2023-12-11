d48bd3ff3b143a19d985d7480c23dcf72aede33f
trinne <torinne@gmail.com>
2023-12-11 02:26:32 +0200

    Add bash language server

--- after/plugin/mason-lspconfig.lua
+++ after/plugin/mason-lspconfig.lua
@@ -1,3 +1,3 @@
 require("mason-lspconfig").setup {
-    ensure_installed = { "jdtls", "eslint", "tsserver", "lua_ls", "rust_analyzer" },
+    ensure_installed = { "jdtls", "eslint", "tsserver", "bashls", "lua_ls", "rust_analyzer" },
 }
