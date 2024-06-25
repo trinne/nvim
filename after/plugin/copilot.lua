c7ae34d36ad4cc93189a4ea48c1b416f857ea311
Tuomas <torinne@gmail.com>
2024-06-25 09:31:18 +0300

    Formatting and remove unused remaps

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,11 +1,11 @@
 vim.g.copilot_filetypes = {
-    ["*"] = false,
-    lua = true,
-    java = true,
-    javascript = true,
-    jsx = true,
-    typescript = true,
-    tsx = true,
-    sql = true,
-    PostgreSQL = true
+  ["*"] = false,
+  lua = true,
+  java = true,
+  javascript = true,
+  jsx = true,
+  typescript = true,
+  tsx = true,
+  sql = true,
+  PostgreSQL = true,
 }
--- lua/trinne/remap.lua
+++ lua/trinne/remap.lua
@@ -6,7 +6,7 @@ vim.keymap.set("n", "<leader>pf", vim.cmd.NvimTreeFindFile) -- vim.cmd.Ex
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- keep the cursor where it was 
+-- keep the cursor where it was
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- keep the cursor in the middle of the screen
@@ -16,22 +16,20 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- paste, but dont yank the visually selected studff
-vim.keymap.set("x", "<leader>p", "\"_dPdNzzzv")
+vim.keymap.set("x", "<leader>p", '"_dPdNzzzv')
 
 -- yank to system clipboard (+register) asbojornHaland
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
 
 -- delete into the void register
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
 
 vim.keymap.set("n", "Q", "<nop>")
 
-vim.keymap.set("n", "ck", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "cj", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "lk", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "lj", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<M-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<M-j>", "<cmd>cprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
